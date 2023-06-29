2ec0b59b5ab181751f9ded23ade32b33285d55f9
antidoteShaurya <shaurya.7.chamoli@gmail.com>
2023-06-29 22:55:12 +0530

    Initial commit with basic Spring Boot setup

---
+++ src/main/java/com/shaurya/tradingbot/tradingbot/TradingbotApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.tradingbot.tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaurya/tradingbot/tradingbot/TradingbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.tradingbot.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
